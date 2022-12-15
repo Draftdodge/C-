e0adf2c4a2734cd098afcd78281ecbe2deb5e3ea
pavel.v <vaninen@ya.ru>
2022-12-15 22:29:08 +0300

    lesson 1

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.Writeline("Введите целое число:");
+int number = Convert.ToInt32(Console.Readline());
+int square = number * number;
+Console.Writeline($"Квадрат числа {number} = {square}");
---
+++ Ex002/Program.cs
@@ -0,0 +1,49 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+Console.WriteLine("Введите число от 1 до 7 и программа выведет название соответствующего дня недели");
+int day_number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if ((day_number > 0)&&(day_number < 8))
+{
+    do
+    {
+        if (day_number == i)
+        {
+            if (day_number == 1)
+            {
+                Console.WriteLine($"цифре {day_number} соответствует день недели Понедельник");
+            }
+            if (day_number == 2)
+            {
+                Console.WriteLine($"цифре {day_number} соответствует день недели Вторник");
+            }
+            if (day_number == 3)
+            {
+                Console.WriteLine($"цифре {day_number} соответствует день недели Среда");
+            }
+            if (day_number == 4)
+            {
+                Console.WriteLine($"цифре {day_number} соответствует день недели Четверг");
+            }
+            if (day_number == 5)
+            {
+                Console.WriteLine($"цифре {day_number} соответствует день недели Пятница");
+            }
+            if (day_number == 6)
+            {
+                Console.WriteLine($"цифре {day_number} соответствует день недели Суббота");
+            }
+            if (day_number == 7)
+            {
+                Console.WriteLine($"цифре {day_number} соответствует день недели Воскресенье");
+            }
+        }
+        i = i + 1;
+    } while ((i == day_number)||(i == 8));
+}
+else
+{
+    Console.WriteLine($"Введенное число {day_number} не соответствует дню недели");    
+}
