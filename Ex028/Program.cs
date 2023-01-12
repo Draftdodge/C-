b8fff15a2cbb4f3901f99e9ea542a7cf20aadf76
pavel.v <vaninen@ya.ru>
2023-01-12 22:30:42 +0300

    Семинар 4

---
+++ Ex028/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int productNumbers = ProductNumbers(number); ;
+    Console.WriteLine("Факториал числа " + number + " = " + productNumbers);
+}
+else Console.WriteLine("Введеное число меньше либо равно 0");
+
+int ProductNumbers(int num)
+{
+    int res = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked  // проверка на переполнение
+        {
+            res = res * i;
+        }
+    }
+    return res;
+}
---
+++ Ex030/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+int[] array = new int[8];
+//int[] array1 = new int[8]{1,3,4,5,6,7,8,9};
+//int[] array1 = new int[]{1,3,4,5,6,7,8,9};
+//int[] array1 = new []{1,3,4,5,6,7,8,9};
+//int[] array1 = {1,3,4,5,6,7,8,9};
+
+FillArray(array);
+
+OutArray(array);
+
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random rnd = new Random();
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+
+
+void OutArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+
+}
